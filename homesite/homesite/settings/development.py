16d137307a28fde4f199c49a2d9379cfd041f097
Maxim Britvin <maksbritvin@gmail.com>
2024-01-10 20:15:56 +0300

    Revert "add production and development settings"
    This reverts commit 24162fc9c2ab0bc9ad81d268791af24a4aeaf88f.

--- homesite/homesite/settings/development.py
+++
@@ -1,8 +0,0 @@
-from homesite.settings.common import *
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR.parent / 'dev.sqlite3',
-    }
-}
